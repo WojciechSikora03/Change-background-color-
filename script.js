76a80fa69bd2a0219a8872563466e8d5c69a15bd
WojciechSikora03 <Wojciech.sikora@poczta.onet.pl>
2023-02-01 17:15:29 +0100

    created button to change background color with javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+var changeColorButton = document.getElementById("changeColorButton");
+
+changeColorButton.addEventListener("click", function(){
+    document.querySelector("body").style.backgroundColor="red";
+})
